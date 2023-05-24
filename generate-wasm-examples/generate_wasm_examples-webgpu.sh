71dda52397d68376878dd5447cb7fcfce40e93f2
Martin Lysell <martande@gmail.com>
2023-05-24 16:28:16 +0200

    Fix some things to make local build simpler

--- generate-wasm-examples/generate_wasm_examples-webgpu.sh
+++ generate-wasm-examples/generate_wasm_examples-webgpu.sh
@@ -5,5 +5,7 @@ cd bevy-main
 git remote add origin https://github.com/bevyengine/bevy
 git pull --depth=1 origin main
 
+cargo run -p example-showcase -- build-web-gpu-examples --content-folder content --website-hacks --optimize-size
 cargo run -p example-showcase -- build-website-list --content-folder content
-mv content ../../content/examples-webgpu
+
+# mv content ../../content/examples-webgpu
